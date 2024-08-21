832b6bfd342b76b932d08866dea53598e9accfab
srisailam nisrayani <s.nisrayaniraj@gmail.com>
2024-08-21 23:57:08 +0530

    removed fixed position to sidebar. Therefore, no need of margin left to main content

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,9 @@
-export function SideBar({ availableCredits }) {
+export function SideBar({ availableCredits }) { 
   return (
     <div>
       <aside
         id="cta-button-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        class="top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
         <div class="h-full px-3 py-4 overflow-y-auto bg-blue-950 flex flex-col justify-between">
